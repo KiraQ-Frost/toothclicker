8834b7b270f77c46a3b528a5f8dcc10a2cf19d97
KiraQ-yeet <77141733+KiraQ-yeet@users.noreply.github.com>
2023-10-08 00:35:08 -0600

    Fixed Javascript
    Made more functions, fixed localstorage saving

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-var teeth = localStorage.getItem("teeth") | 0;
-var tps = localStorage.getItem("tps") | 0;
-var tpc = localStorage.getItem("tpc") | 1;
+var teeth = localStorage.getItem("teeth") != null? Number(localStorage.getItem("teeth")) : 0;
+var tps = localStorage.getItem("tps") != null? Number(localStorage.getItem("tps")) : 0;
+var tpc = localStorage.getItem("tpc") != null? Number(localStorage.getItem("tpc")) : 1;
 
-var poofCost = localStorage.getItem("poofCost") | 69;
-var poofs = localStorage.getItem("poofs") | 0;
-var sipCost = localStorage.getItem("sipCost") | 100;
-var sips = localStorage.getItem("sips") | 0;
-var lacelessCost = localStorage.getItem("lacelessCost") | 1000;
-var laceless = localStorage.getItem("laceless") | 0;
+var poofCost = localStorage.getItem("poofCost") != null? Number(localStorage.getItem("poofCost")) : 69;
+var poofs = localStorage.getItem("poofs") != null? Number(localStorage.getItem("poofs")) : 0;
+var sipCost = localStorage.getItem("sipCost") != null? Number(localStorage.getItem("sipCost")) : 100;
+var sips = localStorage.getItem("sips") != null? Number(localStorage.getItem("sips")) : 0;
+var lacelessCost = localStorage.getItem("lacelessCost") != null? Number(localStorage.getItem("lacelessCost")) : 1000;
+var laceless = localStorage.getItem("laceless") != null? Number(localStorage.getItem("laceless")) : 0;
 
 function count() {
     teeth += tpc;
-    document.getElementById("teethCount").innerText = teeth;
+    updateTeeth();
+
     if(teeth >= 249600000000) {
         document.getElementById("tooManyTeeth").innerText = "You have collected all the teeth";
     }
@@ -27,10 +28,7 @@ function buyPoof() {
         } else {
             tps++;
         }
-
-        document.getElementById("teethCountPerSecond").innerText = tps;
-        document.getElementById("poofCost").innerText = poofCost;
-        document.getElementById("poofNum").innerText = poofs;
+        updateText();
     }
 }
 
@@ -44,10 +42,7 @@ function buySip() {
         } else {
             tpc++;
         }
-
-        document.getElementById("teethCountPerClick").innerText = tpc;
-        document.getElementById("sipCost").innerText = sipCost;
-        document.getElementById("sipNum").innerText = sips;
+        updateText();
     }
 }
 
@@ -59,29 +54,11 @@ function buyLaceless() {
         teeth -= lacelessCost;
         lacelessCost *= 2;
         
-        document.getElementById("teethCountPerSecond").innerText = tps;
-        document.getElementById("teethCountPerClick").innerText = tpc;
-        document.getElementById("lacelessCost").innerText = lacelessCost;
-        document.getElementById("lacelessNum").innerText = laceless;
+        updateText();
     }
 }
 
-setInterval(function() {
-    teeth += tps;
-
-    document.getElementById("teethCount").innerText = teeth;
-
-    document.getElementById("teethCountPerSecond").innerText = tps;
-    document.getElementById("poofCost").innerText = poofCost;
-    document.getElementById("poofNum").innerText = poofs;
-
-    document.getElementById("teethCountPerClick").innerText = tpc;
-    document.getElementById("sipCost").innerText = sipCost;
-    document.getElementById("sipNum").innerText = sips;
-
-    document.getElementById("lacelessCost").innerText = lacelessCost;
-    document.getElementById("lacelessNum").innerText = laceless;
-
+function saveProgress() {
     localStorage.setItem("teeth", teeth);
     localStorage.setItem("tps", tps);
     localStorage.setItem("tpc", tpc);
@@ -91,5 +68,41 @@ setInterval(function() {
     localStorage.setItem("sips", sips);
     localStorage.setItem("lacelessCost", lacelessCost);
     localStorage.setItem("laceless", laceless);
-    
+}
+
+function resetProgress() {
+    teeth = 0;
+    tps = 0;
+    tpc = 1;
+    poofCost = 69;
+    poofs = 0;
+    sipCost = 100;
+    sips = 0;
+    lacelessCost = 1000;
+    laceless = 0;
+
+    saveProgress();
+    updateText();
+
+}
+
+function updateText() {
+    document.getElementById("teethCountPerSecond").innerText = tps;
+    document.getElementById("teethCountPerClick").innerText = tpc;
+    document.getElementById("sipCost").innerText = sipCost;
+    document.getElementById("sipNum").innerText = sips;
+    document.getElementById("poofCost").innerText = poofCost;
+    document.getElementById("poofNum").innerText = poofs;
+    document.getElementById("lacelessCost").innerText = lacelessCost;
+    document.getElementById("lacelessNum").innerText = laceless;
+}
+
+function updateTeeth() {
+    document.getElementById("teethCount").innerText = teeth;
+}
+
+setInterval(function() {
+    teeth += tps;
+    updateTeeth();
+    saveProgress();
 }, 1000)
